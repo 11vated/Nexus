8d8d13813edbbd07c13e531fffc2dd6e14c2dc78
kahlilstephens415-art <kahlil.stephens415@gmail.com>
2026-04-28 14:33:34 -0400

    feat: Add core module exports

---
+++ src/nexus/core/__init__.py
@@ -0,0 +1,18 @@
+"""Core agent system for Nexus."""
+from nexus.core.profound_system import (
+    ProfoundSystem,
+    AutonomousOrchestrator,
+    OllamaModel,
+    AgentRole,
+    TaskStatus,
+    Task,
+)
+
+__all__ = [
+    "ProfoundSystem",
+    "AutonomousOrchestrator",
+    "OllamaModel",
+    "AgentRole",
+    "TaskStatus",
+    "Task",
+]
