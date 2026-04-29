a8c43a47ac21908d6c0eafb756c9aa09c621f5e5
11vatedTech LLC <11vated11@gmail.com>
2026-04-29 21:49:16 +0000

    feat: .nexus/ plugin configuration system + nexus init CLI command
    - PluginConfigLoader: discovers/loads project config from .nexus/
    - ProjectConfig, StanceConfig, HookConfig, WatcherConfig, ModelConfig dataclasses
    - init() scaffolds .nexus/ with config.yaml, rules.md, stances/, hooks/, sessions/
    - Supports inline watchers/hooks in config.yaml + separate YAML files in subdirectories
    - nexus init CLI command to initialize .nexus/ directory
    - 34 new tests (674 total passing)

--- src/nexus/config/__init__.py
+++ src/nexus/config/__init__.py
@@ -1,3 +1,4 @@
 from .settings import config, NexusConfig
+from .plugin_config import PluginConfigLoader, ProjectConfig
 
-__all__ = ["config", "NexusConfig"]
+__all__ = ["config", "NexusConfig", "PluginConfigLoader", "ProjectConfig"]
